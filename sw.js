99544f4091e301bc49ec4027f0d25c78404d4f1e
gsh0905 <gsh090509@gmail.com>
2026-05-22 19:35:41 +0900

    Add files via upload
    Convert to PWA - add manifest, service worker, icons

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE = 'stan-korean-v1';
+const PRECACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.svg',
+  './icons/icon-512.svg',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(PRECACHE)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(hit =>
+      hit || fetch(e.request).then(res => {
+        if (res.ok) caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+        return res;
+      }).catch(() => caches.match('./index.html'))
+    )
+  );
+});
